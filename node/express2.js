30f97bf5407c1b34a71a85323a476fdc0b09cfcc
Bri Lance <bri@bri-lance.net>
2017-08-23 14:15:55 -0400

    clean up unwanted directories

--- node/express2.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-
-const app=express();
-app.set('view engine','jade');
-
-app.get('/',(req,res)=>res.render('index',{title:'Guru99',message:'Welcome to node.js and jade'}) );
-const server = app.listen(3000,function(){});
