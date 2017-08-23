5f4a5eb8622e95cc12578ac9f90078bd3ff93c03
Bri Lance <bri@bri-lance.net>
2017-08-23 13:57:35 -0400

    retrieve route info from SEPTA API

---
+++ septaMap/routes/info.js
@@ -0,0 +1,16 @@
+const request = require('request'); // "Request" library
+const express = require('express');
+const router = express.Router();
+
+router.get('/route/:id', function(req, res, next){
+    const routeID = req.params.id;
+    const options = {
+        url: `http://www3.septa.org/beta/TransitView/${routeID}`,
+        json: true
+    };
+    request.get(options, function(error, response, body) {
+        res.json(body);
+    });
+});
+
+module.exports = router;
