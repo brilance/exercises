fe3f054a389bf11d23d20a13ab158d9ffa6428fd
Bri Lance <bri@bri-lance.net>
2017-08-23 13:57:19 -0400

    septaMap initial commit

---
+++ septaMap/server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+const info = require('./routes/info');
+
+//middleware
+app.use(logger('dev'));
+
+app.use('/api/v1/', info);
+//app.use(express.static(__dirname + '/client/src/'));
+
+//error handling
+app.use(function(req, res, next){
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+//create server
+const server = app.listen(process.env.PORT || 3000, () => {
+	const host = 'localhost';
+	const port = server.address().port;
+	console.log(`App listening at http://${host}:${port}`, host, port);
+});
+
+module.exports = app;
