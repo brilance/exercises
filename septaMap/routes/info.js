3812d94f9b0d5368ec5126d25355a97d249c41b6
Bri Lance <bri@bri-lance.net>
2017-08-24 14:32:43 -0400

    service to return the closest stops based on lat/long

--- septaMap/routes/info.js
+++ septaMap/routes/info.js
@@ -22,4 +22,50 @@ router.get('/route/:id', function(req, res, next){
     });
 });
 
+router.get('/route/:id/stop/', function(req, res, next){
+    const routeID = req.params.id;
+    const lat = req.query.lat;
+    const long = req.query.long
+    const options = {
+        url: `http://www3.septa.org/hackathon/Stops/${routeID}`,
+        json: true
+    };
+    let longDelta = 10;
+    let latDelta = 10;
+    let closestLatIdx = -1;
+    let closestLongIdx = -1;
+    let idx = -1;
+    request.get(options, function(error, response, body) {
+        if (body){
+            for (item of body){
+                idx++;
+                const bodyLat = item["lat"];
+                const bodyLong = item["lng"];
+                if (Math.abs(lat-bodyLat) < latDelta){
+                    latDelta = Math.abs(lat-bodyLat);
+                    closestLatIdx = idx;
+                }
+                if (Math.abs(long-bodyLong) < longDelta){
+                    longDelta = Math.abs(long-bodyLong);
+                    closestLongIdx = idx;
+                }
+            }
+            if (closestLatIdx == closestLongIdx){
+                res.json(body[closestLatIdx]);
+            }
+            else{
+                const response = [body[closestLatIdx], body[closestLongIdx]];
+                res.json(response);
+            }
+        }
+        else{
+            const error = {
+                error:true,
+                errorMsg:"Invalid route"
+            }
+            res.json(error);
+        }
+    });
+});
+
 module.exports = router;
